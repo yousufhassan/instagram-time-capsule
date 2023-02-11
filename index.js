4660c043ac24c73cb1fbd31f2b112125e3753637
Yousuf Hassan <yousufhassan209@gmail.com>
2023-02-11 10:35:45 -0500

    Created initial terminal prototype

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
-import { sendFile } from "express/lib/response";
-import { PassThrough } from "stream";
-import { ESMap, Map } from "typescript";
-
-const { assert } = require('console');
-const express = require('express');
-const app = express();
-const port = 3000;
-
+"use strict";
+exports.__esModule = true;
+var assert = require('console').assert;
+var express = require('express');
+var app = express();
+var port = 3000;
 // Start web app
 //Idiomatic expression in express to route and respond to a client request
 // app.get('/', (req, res) => {        //get requests to the root ("/") will route here
 //     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
 //                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 // });
-
 // app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
 //     console.log(`Now listening on port ${port}`); 
 // });
-
-
-function customPrint(message: JSON): void {
-    let senderName = message['sender_name'];
-    let datetime = new Date(message['timestamp_ms']);
-    let date = datetime.toLocaleString('default', {month: 'short',
-                                                   day: '2-digit', year: 'numeric'});
-    let time = datetime.toLocaleString('default', {hour: 'numeric', minute: '2-digit'});
-
-    let content = message['content'];
-
-    console.log(`[${date} - ${time}] [${senderName}]: ${content}`);
+function customPrint(message) {
+    var senderName = message['sender_name'];
+    var datetime = new Date(message['timestamp_ms']);
+    var date = datetime.toLocaleString('default', { month: 'short',
+        day: '2-digit', year: 'numeric' });
+    var time = datetime.toLocaleString('default', { hour: 'numeric', minute: '2-digit' });
+    var content = message['content'];
+    console.log("[".concat(date, " - ").concat(time, "] [").concat(senderName, "]: ").concat(content));
 }
-
-
 /**
  * Function to retrieve all messages from a given file path.
- * 
+ *
  * @param filepath - A string representing the file path to retrieve messages from.
  * @returns An array of JSON, where each JSON represents a message.
  */
-function getAllMessages(filepath: string): JSON[] {
-    let data = require(filepath);
-    let messages = data.messages;
+function getAllMessages(filepath) {
+    var data = require(filepath);
+    var messages = data.messages;
     return messages;
 }
-
 /**
  * Function that splits a given array of messages into conversations by day.
- * 
+ *
  * @param messages Array of messages to be split into conversations by their date.
  * @returns Map[dateAsString, ArrayOfMessages]
  */
-function splitConversationsByDay(messages: JSON[]): ESMap<string, JSON[]> {
-    let conversationsMap = new Map();
-    messages.forEach(message => {
-        let date = new Date(message['timestamp_ms']).toDateString().slice(4);
+function splitConversationsByDay(messages) {
+    var conversationsMap = new Map();
+    messages.forEach(function (message) {
+        var date = new Date(message['timestamp_ms']).toDateString().slice(4);
         if (conversationsMap.has(date)) {
             conversationsMap.get(date).push(message);
         }
@@ -63,41 +52,32 @@ function splitConversationsByDay(messages: JSON[]): ESMap<string, JSON[]> {
     });
     return conversationsMap;
 }
-
 /**
  * Function to retrieve a conversation by date from a given map of conversations.
- * 
+ *
  * @param conversationsMap An ESMap object representing a map of conversations, where each key is
  *                           a date string and each value is the Array of messages sent
  *                           on that date.
  * @param date A string representing the date for which the conversation is to be retrieved.
  * @returns An array of ESMap objects, where each ESMap object represents a message in the
- *          retrieved conversation. 
+ *          retrieved conversation.
  *          If no conversation is found for the given date, an empty array is returned.
  */
-function getConversationByDate(conversationsMap: ESMap<string, JSON[]>,
-                               date: string): JSON[] {
+function getConversationByDate(conversationsMap, date) {
     if (!(conversationsMap.has(date))) {
         console.log('No conversations found for ' + date);
         return [];
     }
-    return conversationsMap.get(date)!;
+    return conversationsMap.get(date);
 }
-
-
-let messages = getAllMessages('./original_data/message_1.json');
-
-let conversationsMap = splitConversationsByDay(messages);
-let temp = getConversationByDate(conversationsMap, "Jan 16 2023")
+var messages = getAllMessages('./original_data/message_1.json');
+// console.log(messages);
+var conversationsMap = splitConversationsByDay(messages);
+var temp = getConversationByDate(conversationsMap, "Jan 16 2023");
 console.log(temp);
-
-
-for (let i = 0; i < messages.length - 1; i++) {
-    assert(messages[i]['timestamp_ms'] > messages[i+1]['timestamp_ms']);
+for (var i = 0; i < messages.length - 1; i++) {
+    assert(messages[i]['timestamp_ms'] > messages[i + 1]['timestamp_ms']);
 }
-
-messages.forEach(message => {
+messages.forEach(function (message) {
     customPrint(message);
 });
-
-
