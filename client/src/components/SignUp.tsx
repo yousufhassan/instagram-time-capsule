eac545b2f2c71f9a11c0aad108b9be33941d42f4
Yousuf Hassan <yousufhassan209@gmail.com>
2023-02-16 18:24:21 -0500

    Changed SignUp to be a function component and completed its implementation.

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ChangeEvent } from 'react';
+import React, { Component, ChangeEvent, useState } from 'react';
 import axios from 'axios';
 // import { useState } from 'react';
 import '.././styles/general.css';
@@ -7,25 +7,19 @@ import '.././styles/form.css';
 import '.././styles/button.css';
 import { Link, useNavigate } from "react-router-dom";
 
-class SignUp extends Component {
-    constructor(props: any) {
-        super(props);
 
-        this.state = {
-            username: '',
-            password: ''
-        }
 
-        this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
-        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
-        this.sumbitForm = this.sumbitForm.bind(this);
-    }
+function SignUp() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    sumbitForm(e: React.FormEvent<HTMLFormElement>) {
+    const sumbitForm = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        axios.post('http://localhost:8000/createUser', this.state)
+        axios.post('http://localhost:8000/createUser', {username, password})
             .then((response) => {
                 console.log(response.data)
+                navigate('/home')
             })
             .catch(function (error) {
                 console.log(error);
@@ -35,54 +29,36 @@ class SignUp extends Component {
             });
     }
 
-    usernameChangeHandler(e: ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            username: e.target.value
-        })
-    }
-
-    passwordChangeHandler(e: ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            password: e.target.value
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <div id="app-name">Instagram Time Capsule</div>
-                <div id="form-container" className='flex-col'>
-                    <h2 className='main-sage-text'>Create an account</h2>
-                    <form onSubmit={this.sumbitForm} className="flex-col regular-spacing">
-                        <div className="form-section">
-                            <label className='main-sage-text' htmlFor="username">Username</label>
-                            <input required minLength={6} name='username' className='light-sage-bg white-text'
-                                placeholder='Enter a cool username' type="text"
-                                onChange={(e) => this.usernameChangeHandler(e)} />
-                        </div>
-                        <div className="form-section">
-                            <label className='main-sage-text' htmlFor="password">Password</label>
-                            <input required minLength={8} name='password' className='light-sage-bg white-text'
-                                placeholder='Enter a unique password' type="text"
-                                onChange={(e) => this.passwordChangeHandler(e)} />
-                        </div>
-                        <div className="form-section flex-row center">
-                            <button id='submit-btn' className='btn flex-row center main-sage-bg white-text'>
-                                Create account
-                            </button>
-                        </div>
-                    </form>
-                    <p>Already have an account?
-                        <Link to="/login" className='no-underline main-sage-text'> Log in</Link>
-                    </p>
-                </div>
+    return (
+        <div>
+            <div id="app-name">Instagram Time Capsule</div>
+            <div id="form-container" className='flex-col'>
+                <h2 className='main-sage-text'>Create an account</h2>
+                <form onSubmit={sumbitForm} className="flex-col regular-spacing">
+                    <div className="form-section">
+                        <label className='main-sage-text' htmlFor="username">Username</label>
+                        <input required minLength={6} name='username' className='light-sage-bg white-text'
+                            placeholder='Enter a cool username' type="text"
+                            onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className="form-section">
+                        <label className='main-sage-text' htmlFor="password">Password</label>
+                        <input required minLength={8} name='password' className='light-sage-bg white-text'
+                            placeholder='Enter a unique password' type="password"
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className="form-section flex-row center">
+                        <button id='submit-btn' className='btn flex-row center main-sage-bg white-text'>
+                            Create account
+                        </button>
+                    </div>
+                </form>
+                <p>Already have an account?
+                    <Link to="/login" className='no-underline main-sage-text'> Log in</Link>
+                </p>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-
-
-
 
 export default SignUp;
