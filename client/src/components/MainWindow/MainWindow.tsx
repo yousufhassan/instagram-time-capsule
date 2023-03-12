c0d715a2834b5e492b3939c6ba52b55c4ff36189
Yousuf Hassan <yousufhassan209@gmail.com>
2023-03-11 20:29:48 -0500

    Created ChatItem Component

--- client/src/components/MainWindow/MainWindow.tsx
+++ client/src/components/MainWindow/MainWindow.tsx
@@ -4,14 +4,35 @@ import '../../styles/general.css';
 import './MainWindow.css';
 // import './Home.css';
 import UploadFile from '../UploadFile/UploadFile';
+import { useState } from 'react';
 
+
+function ChatItem({ chatTitle, numMessages }: { chatTitle: string, numMessages: number }) {
+    const colors = ['#512C2C', '#586E52', '#3C4E64', '#D9D9D9'];
+    const bgColor = colors[Math.floor(Math.random() * colors.length)];
+    return (
+        <div>
+            <div className="flex-row center chat-item-container">
+                <span className='flex-row center chat-item-img' style={{backgroundColor: bgColor}}>
+                </span>
+                <div className="chat-item-details">
+                    <h3 className='no-margin white-text'>{chatTitle}</h3>
+                    <p className='thin white-text'>{numMessages} messages</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 function MainWindow() {
+    const [chatList, setChatList] = useState([]);
 
-    const fileUploadCallback = (chatData: JSON) => {
-        console.log(chatData);
+    const fileUploadCallback = (chatData: { chatTitle: string, numMessages: number }) => {
+        // console.log(chatData.chatTitle);
+        // let newChat = <ChatItem chatTitle={chatData.chatTitle} numMessages={chatData.numMessages} />
+        // setNewChat(chatData);
     }
-    
+
     return (
         <div>
             <div id="main-container" className='flex-row'>
@@ -20,12 +41,16 @@ function MainWindow() {
                         <h3 className='light-grey-text'>Conversations</h3>
                     </div>
                     <div id="conversation-list">
-                        <div className='conversation-item'>item</div>
-                        <div className='conversation-item'>item</div>
-                        <div className='conversation-item'>item</div>
+                        {/* <ul> */}
+                        <ChatItem chatTitle='Jim Halpert' numMessages={3209} />
+                        <ChatItem chatTitle='Pam Beesly' numMessages={850} />
+                        {/* <li>item1</li> */}
+                        {/* <li>item2</li> */}
+                        {/* <li>item3</li> */}
+                        {/* </ul> */}
                     </div>
                     <div id="conversation-footer">
-                        <UploadFile chatDataCallback={fileUploadCallback}/>
+                        <UploadFile chatDataCallback={fileUploadCallback} />
                     </div>
                 </div>
                 <div id="chat-panel" className='flex-col light-grey-bg'>
