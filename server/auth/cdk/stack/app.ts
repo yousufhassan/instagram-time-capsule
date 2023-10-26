db7a6b80e4f60b9629a0237883633c5c1f14407d
Yousuf Hassan <yousufhassan209@gmail.com>
2023-10-26 00:44:19 -0700

    fixed a little oopsie

--- server/auth/cdk/stack/app.ts
+++ server/auth/cdk/stack/app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { App } from "aws-cdk-lib";
-import { AuthStack } from "./AuthStack";
+import { AuthStack } from "./stack";
 
 const app = new App();
 new AuthStack(app, "AuthStack");
