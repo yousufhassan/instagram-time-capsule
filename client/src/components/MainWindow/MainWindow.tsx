3ecb996385dda59055a2738cb091a230ee1a2a02
Yousuf Hassan <yousufhassan209@gmail.com>
2023-11-14 01:10:27 -0800

    Allowed conversation panel display to be toggled

--- client/src/components/MainWindow/MainWindow.tsx
+++ client/src/components/MainWindow/MainWindow.tsx
@@ -8,14 +8,21 @@ function MainWindow() {
     // eslint-disable-next-line
     const user = JSON.parse(localStorage.getItem("user")!);
     const [activeChat, setActiveChat] = useState(Object());
+    const [isPanelOpen, setPanelOpen] = useState(true);
 
     return (
         <div id="main-container" className="flex-row">
-            <div id="conversation-panel-container" className="flex-col dark-grey-bg">
-                <ConversationPanel activeChat={activeChat} setActiveChat={setActiveChat} />
-            </div>
+            {/* <div id="conversation-panel-container" className="flex-col dark-grey-bg"> */}
+            <ConversationPanel
+                activeChat={activeChat}
+                setActiveChat={setActiveChat}
+                isPanelOpen={isPanelOpen}
+                setPanelOpen={setPanelOpen}
+            />
+            {/* <span id="sidebar-btn">{">"}</span> */}
+            {/* </div> */}
             <div id="chat-panel-container" className="flex-col light-grey-bg">
-                <ChatPanel activeChat={activeChat} />
+                <ChatPanel activeChat={activeChat} isPanelOpen={isPanelOpen} setPanelOpen={setPanelOpen} />
             </div>
         </div>
     );
